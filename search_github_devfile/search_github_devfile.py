a44784dd31c840b2be6082cf244706ce3ca59167
Maysun J Faisal <maysunaneek@gmail.com>
2024-02-16 17:25:39 -0500

    Update search filter & try/catch block
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- search_github_devfile/search_github_devfile.py
+++ search_github_devfile/search_github_devfile.py
@@ -9,12 +9,17 @@ def parse_devfile_url(url):
     
    response = requests.get(url, allow_redirects=True)
    content = response.content.decode("utf-8")
-   data = yaml.safe_load(content)
+   try:
+      data = yaml.safe_load(content)
+   except:
+      return None
    
    if "schemaVersion" in data:
       schemaVersion = data["schemaVersion"]
       if schemaVersion == "2.0.0" or schemaVersion == "2.1.0" or schemaVersion == "2.2.0":
          print("devfile has schemaVersion ", schemaVersion, " and will be chosen for training the model")
+         return data
+   return None
 
 def search_github():
     
@@ -26,18 +31,22 @@ def search_github():
    # pygithub object
    g = Github(auth=auth)
    
-   codes = g.search_code(query="filename:devfile.yaml")
+   codes = g.search_code(query="filename:devfile.yaml schemaVersion")
    
    print("Total number of files titled devfile.yaml after search: ", codes.totalCount, end="\n")
    print("-----")
    
    x = 0
+   devfile_data_list = []
    for code in codes:
        x += 1
        print("Repo: ",  code.repository)
        print("File URL: ", code.html_url)
        print("Download URL: ", code.download_url)
-       parse_devfile_url(code.download_url)
+       parsed_data = parse_devfile_url(code.download_url)
+       if parsed_data is not None:
+          print("devfile content has been downloaded")
+          devfile_data_list.append(devfile_data_list)
        print("-----")
        if x == 24:
            break
